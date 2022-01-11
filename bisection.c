e30ce8c8d4b651cbb610c61368fad5d6c306762b
paes-mario <96839726+paes-mario@users.noreply.github.com>
2022-01-10 21:45:57 -0300

    Update bisection.c

--- bisection.c
+++ bisection.c
@@ -9,28 +9,24 @@ tolerance of 10^-5
 #include <stdio.h>
 #include <math.h>
 
-double f(double x);
+double f(double x) {return pow(x,3)-x-1;}
 
 int main(void) {
+    
     double a = 1.0, b = 2.0, tol = 1e-5, m;
     int it = 0; // iterations counter
+    
     do {
         m = (a + b)/2.0;
-        if (f(a)*f(m)<0.0) {
-            b = m;
-        }
-        else {
-            a = m;
-        }
+        
+        if (f(a)*f(m)<0.0) {b = m;}
+        else {a = m;}
+        
         it++;
     } while(fabs(f(m))>tol);
 
     printf("The root of the polynomial is: %f\n", m);
     printf("The number of interations is: %d", it);    
-}
-
-double f(double x) {
-    return pow(x,3)-x-1;
 }
 
 /* Output:
